5a402cc418e8bd6a8594e627d3ae5a6dd7224205
kfiddle <kenjfiddle@gmail.com>
2021-09-30 17:10:48 -0400

    various

--- src/main/java/com/example/demo/controllers/PPP_Rest.java
+++ src/main/java/com/example/demo/controllers/PPP_Rest.java
@@ -1,7 +1,9 @@
 package com.example.demo.controllers;
 
 
+import com.example.demo.junctionTables.PerformancePiece;
 import com.example.demo.junctionTables.PerformancePiece_Player;
+import com.example.demo.repositories.PerformancePieceRepository;
 import com.example.demo.repositories.PerformancePiece_PlayerRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.Collection;
 
 @CrossOrigin
@@ -18,6 +21,9 @@ public class PPP_Rest {
     @Resource
     PerformancePiece_PlayerRepository pppRepo;
 
+    @Resource
+    PerformancePieceRepository performancePieceRepo;
+
 
     @PostMapping("/add-ppp")
     public Collection<PerformancePiece_Player> addPPPToDatabase(@RequestBody PerformancePiece_Player incomingPPP) {
@@ -25,6 +31,16 @@ public class PPP_Rest {
         pppRepo.save(pppToAdd);
         System.out.println(pppToAdd.getPerformancePiece().getPiece().getTitle() + "    " + pppToAdd.getInstrumentEnum());
         return (Collection<PerformancePiece_Player>) pppRepo.findAll();
+    }
+
+    @PostMapping("/get-ppps-from-pp")
+    public Collection<PerformancePiece_Player> getAllPPPsFromPerformancePiece(@RequestBody PerformancePiece incomingPP) {
+        Collection<PerformancePiece_Player> pppsToReturn = new ArrayList<>();
+        if(performancePieceRepo.findById(incomingPP.getId()).isPresent()) {
+            pppsToReturn.addAll(pppRepo.findAllByPerformancePiece(incomingPP));
+        }
+        return pppsToReturn;
+
     }
 
 
--- src/main/java/com/example/demo/repositories/PerformancePiece_PlayerRepository.java
+++ src/main/java/com/example/demo/repositories/PerformancePiece_PlayerRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.repositories;
 
+import com.example.demo.junctionTables.PerformancePiece;
 import com.example.demo.junctionTables.PerformancePiece_Player;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+
 public interface PerformancePiece_PlayerRepository extends CrudRepository<PerformancePiece_Player, Long> {
+    Collection<PerformancePiece_Player> findAllByPerformancePiece(PerformancePiece incomingPP);
 }
